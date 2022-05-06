bc8ec4755eed4344263bb5de662055659a015255
Julien ichard WCS <julien.y.richard@gmail.com>
2022-05-06 15:14:13 +0200

    Correction live

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import papa from 'papaparse';
 import './App.css';
 
 function App() {
+  const [cars, setCars] = useState([])
+  const prepareData = data => {
+    const json = data.map((line, index) => {
+      if (index > 0) {
+        let obj = {};
+        data[0].forEach((key, j) => (obj = { ...obj, [key]: line[j] }));
+        return obj;
+      }
+    })
+    json.shift();
+    setCars(json)
+  }
+  useEffect(() => {
+    fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTDwbHakl1R61Lba_U6N-5Yh6ovQx_MzBXqG9LWNRkT3nPkx1ZRWNbI8poP4lWqcO_cNd5PZyvOeFTj/pub?output=csv')
+      .then(result => result.text())
+      .then(text => papa.parse(text))
+      .then(data => prepareData(data.data))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Google Data</h1>
+      {cars.length > 0 && cars.map(car => <h6 key={car.id}>{car.company_name}</h6>)}
     </div>
   );
 }
